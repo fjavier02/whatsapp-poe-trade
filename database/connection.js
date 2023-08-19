ab01a520e3bad3a8862385251bc56cbf3b3c11e9
fjavier02 <franciscolopez1995@gmail.com>
2023-08-19 18:02:04 -0300

    fix bug for instalation mysql2

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
+import mysql2 from 'mysql2';
 require('dotenv').config()
 
 // Configuración de la conexión a la base de datos
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql', // Cambia esto según tu base de datos (puede ser 'mysql', 'postgres', etc.)
+  dialect: mysql2, // Cambia esto según tu base de datos (puede ser 'mysql', 'postgres', etc.)
 });
 
 module.exports = sequelize;
