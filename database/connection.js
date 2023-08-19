ca2d9c3dd18a030a8c5c4ec8f8c10bfdc994c714
fjavier02 <franciscolopez1995@gmail.com>
2023-08-19 18:05:32 -0300

    new sintax for connection sequelize

--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,16 @@
-const Sequelize = require('sequelize');
-import mysql2 from 'mysql2';
-require('dotenv').config()
+import Sequelize from 'sequelize';
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
 
-// Configuración de la conexión a la base de datos
-const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: mysql2, // Cambia esto según tu base de datos (puede ser 'mysql', 'postgres', etc.)
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql',
+    dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+  }
+)
 
 module.exports = sequelize;
